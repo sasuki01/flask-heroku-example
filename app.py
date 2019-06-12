2962ee4af9406fbd5237da7718d1a6ae29ce39dd
sasuki01 <51392397+sasuki01@users.noreply.github.com>
2019-06-12 09:57:56 +0700

    Update app.py

--- app.py
+++ app.py
@@ -7,8 +7,8 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     """Return homepage."""
-    json_data = {'Hello': 'World!'}
-    return jsonify(json_data)
+#    json_data = {'Hello': 'World!'}
+#    return jsonify(json_data)
   return "นัฐวุฒิ หงษ์กลาย 4 ม.4/1"
 
 if __name__ == '__main__':
